6e7a0682a7b4c5a5904e6c5a32436137112b6ef7
Viktor Ilie <60520772+viktorilie@users.noreply.github.com>
2024-07-11 15:48:57 +0200

    build the color mode switch

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,10 @@ function App() {
           <NavBar />
         </GridItem>
         <Show above="lg">
-          <GridItem area="aside" bg="gold">
-            ASIDE
-          </GridItem>
+          <GridItem area="aside">ASIDE</GridItem>
         </Show>
 
-        <GridItem area="main" bg="dodgerblue">
-          MAIN
-        </GridItem>
+        <GridItem area="main">MAIN</GridItem>
       </Grid>
     </>
   );
