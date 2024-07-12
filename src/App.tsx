351f6599103c1e211e61e067df0bd0efcdbc1550
Viktor Ilie <60520772+viktorilie@users.noreply.github.com>
2024-07-12 12:12:33 +0200

    Display genres

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,15 @@ function App() {
   return (
     <>
       <Grid
-        templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}>
+        templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}
+        templateColumns={{ base: "1fr", lg: "200px 1fr" }}>
         <GridItem area="nav">
           <NavBar />
         </GridItem>
         <Show above="lg">
-          <GenreList />
+          <GridItem area="aside" paddingX={5}>
+            <GenreList />
+          </GridItem>
         </Show>
         <GridItem area="main">
           <GameGrid />
