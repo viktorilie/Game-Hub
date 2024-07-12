6a84027194e04ef057e07c232c9cb9c6d8abb3da
Viktor Ilie <60520772+viktorilie@users.noreply.github.com>
2024-07-12 12:48:49 +0200

    filter games by genre

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -37,7 +37,11 @@ const PlatformIconList = ({ platforms }: Props) => {
     <>
       <HStack marginY={1}>
         {platforms.map((platform) => (
-          <Icon as={iconMap[platform.slug]} color="gray.500" />
+          <Icon
+            key={platform.id}
+            as={iconMap[platform.slug]}
+            color="gray.500"
+          />
         ))}
       </HStack>
     </>
