d4e5c378d649ad3d496307891fd7ce1f00f6c869
Viktor Ilie <60520772+viktorilie@users.noreply.github.com>
2024-07-12 17:11:57 +0200

    implement searching

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Grid, GridItem, HStack, Show } from "@chakra-ui/react";
+import { Box, Flex, Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
@@ -12,6 +12,7 @@ export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
   sortOrder: string;
+  searchText: string;
 }
 
 function App() {
@@ -28,7 +29,11 @@ function App() {
         templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}
         templateColumns={{ base: "1fr", lg: "200px 1fr" }}>
         <GridItem area="nav">
-          <NavBar />
+          <NavBar
+            onSearch={(searchText) =>
+              setGameQuery({ ...gameQuery, searchText })
+            }
+          />
         </GridItem>
         <Show above="lg">
           <GridItem area="aside" paddingX={5}>
