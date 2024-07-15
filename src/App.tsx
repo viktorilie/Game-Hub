003527e7c84b4b5e55079f9d72bb0c45d7bc788a
Viktor Ilie <60520772+viktorilie@users.noreply.github.com>
2024-07-16 00:46:43 +0200

    sidebar expanded to 300px

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
     <>
       <Grid
         templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}
-        templateColumns={{ base: "1fr", lg: "200px 1fr" }}>
+        templateColumns={{ base: "1fr", lg: "300px 1fr" }}>
         <GridItem area="nav">
           <NavBar
             onSearch={(searchText) =>
